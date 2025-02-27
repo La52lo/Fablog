9b2783d567c325d35574c153a69ffa4324d53fb8
La52lo <laszlo.szikszai@outlook.com>
2025-02-27 19:32:50 +0900

    bugfix

--- api/getSequences.js
+++ api/getSequences.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     try {
         const client = new MongoClient(process.env.MONGO_URI);
         await client.connect();
-        const db = client.db("your_db"); // Replace with your database name
+        const db = client.db("gxn-devel"); // Replace with your database name
         const sequences = await db.collection("sequences").find().toArray();
         
         res.status(200).json({ success: true, data: sequences });
