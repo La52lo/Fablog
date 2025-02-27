ce43db7bb746909877fd5c09627486c3c39bb1f5
La52lo <laszlo.szikszai@outlook.com>
2025-02-27 21:42:42 +0900

    testing

---
+++ api/getLogsheetByTitle.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+    var dbName = "logbook";
+	var collName = "logsheets";
+	try {
+        const client = new MongoClient(process.env.MONGO_URI);
+        await client.connect();
+        const db = client.db(dbName); // Replace with your database name
+		const collection = db.collection(collName);
+        const titles = await db.collection.distinct("title");
+        
+        res.status(200).json({ success: true, data: titles });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
