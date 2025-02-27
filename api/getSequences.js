dc742dfcebb37b30f7453e40b5966faa2af9381a
La52lo <laszlo.szikszai@outlook.com>
2025-02-27 21:00:03 +0900

    debug

--- api/getSequences.js
+++ api/getSequences.js
@@ -9,6 +9,6 @@ export default async function handler(req, res) {
         
         res.status(200).json({ success: true, data: sequences });
     } catch (error) {
-        res.status(500).json({ success: false, error: process.env.MONGO_URI });
+        res.status(500).json({ success: false, error: error.message });
     }
 }
