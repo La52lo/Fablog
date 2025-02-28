1f764308a1eb9587df5e230e1fde3dde71960ed6
La52lo <laszlo.szikszai@outlook.com>
2025-02-28 19:25:24 +0900

    debug

--- api/getAllTemplateTitles.js
+++ api/getAllTemplateTitles.js
@@ -9,11 +9,6 @@ export default async function handler(req, res) {
     }
 
 	try {
-        const { title } = req.body;
-        if (!title) {
-            return res.status(400).json({ success: false, error: "Title is required" });
-        }
-
         if (!cachedClient) {
             cachedClient = new MongoClient(process.env.MONGO_URI);
             await cachedClient.connect();
