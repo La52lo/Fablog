55f03d7f3f1bee7d3743933ccccd159f59c9c895
La52lo <laszlo.szikszai@outlook.com>
2025-03-06 17:42:02 +0900

    json

--- api/app.js
+++ api/app.js
@@ -6,8 +6,9 @@ app.use(express.json());
 
 // Test route to verify deployment
 app.get("/", (req, res) => {
-    console.log("Received request: GET /");
-    res.send("Hello from Express on Vercel!");
+    console.log("Sending JSON response...");
+    res.json({ message: "Hello from Express on Vercel!" });
+
 });
 
 // Export the Express app correctly
