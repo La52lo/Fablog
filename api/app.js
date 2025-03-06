1c8829d59417412c827140aed37a04ebe8adef88
La52lo <laszlo.szikszai@outlook.com>
2025-03-06 18:28:09 +0900

    verceljson 5

--- api/app.js
+++ api/app.js
@@ -6,8 +6,12 @@ app.use(express.json());
 
 // Test route to verify deployment
 app.get("/", (req, res) => {
-    console.log("Sending JSON response...");
-    res.json({ message: "Hello from Express on Vercel!" });
+	try{
+		console.log("Sending JSON response...");
+		res.json({ message: "Hello from Express on Vercel!" });
+	}catch(error){
+		res.json({ message: error.message });
+	}	
 
 });
 
