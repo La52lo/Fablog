6f722bbbf1ad785d0487c90180421e78bfff1d00
La52lo <laszlo.szikszai@outlook.com>
2025-03-06 17:14:24 +0900

    export 6

--- api/app.js
+++ api/app.js
@@ -1,22 +1,14 @@
 const express = require("express");
-const serverless = require("serverless-http");
 
 const app = express();
-
-// Middleware to handle JSON
 app.use(express.json());
 
-// Log when a request is received
-app.use((req, res, next) => {
-    console.log(`Received request: ${req.method} ${req.url}`);
-    next();
-});
-
-// Basic test route
 app.get("/", (req, res) => {
     console.log("Sending response...");
-    res.send("Hello from Express on Vercel!");
+    res.status(200).send("Hello from Express without serverless-http!");
 });
 
-// Export the Express app
-module.exports = serverless(app);
+// Export the Express app as a normal function
+module.exports = (req, res) => {
+    app(req, res);
+};
